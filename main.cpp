4b6442a022712dd25ef5d476eb6ac38780560615
Anastasia Volos <voloc338@gmail.com>
2021-09-22 22:10:47 +0300

    logic

--- main.cpp
+++ main.cpp
@@ -4,7 +4,11 @@
 
 int sin(){
     int foo [5] = { 16, 2, 77, 40, 12 };
-    // 5 - 3cos2 => 1,3,4,5,6,7,7,8,4,5,5,4,3,3,2,4,2,2,3,4,4
+    // 5 - 3cos2 => 1,3,4,5,6,7,7,8,4,5,5,4,||3,3,2,4,2,2,3,4,4,34,45,44,34,22,||45,44,12,23,
+    // beresh s 10 po 20 chisla
+    // esli chislo bolshe 4 to pokazaviesh na ekrane
+
+
     int n, result=0;
     for ( n=0 ; n<5 ; ++n )
     {
